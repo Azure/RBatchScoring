89f7e7eafc006cf42dc29c12510197f1fe965e74
angusrtaylor <angusrtaylor@gmail.com>
2019-01-03 23:05:47 +0000

    removed redundant utilities

--- R/utilities.R
+++ R/utilities.R
@@ -1,39 +1,4 @@
 
-read_all_data <- function(filedir) {
-  files2read = list.files(path = file.path(filedir, "data"), pattern="*.csv", full.names = TRUE)
-  files = lapply(files2read, read.csv)
-  do.call(rbind, files)
-}
-
-df2list <- function(store_brand, dat, start = c(0, 0)) {
-  s <- store_brand$store
-  b <- store_brand$brand
-  df <- dat %>% filter(store == s, brand == b)
-  xreg <- df %>%
-    select(
-      price1,
-      price2,
-      price3,
-      price4,
-      price5,
-      price6,
-      price7,
-      price8,
-      price9,
-      price10,
-      price11,
-      deal,
-      feat
-    )
-  list(store = s, brand = b,
-       series =  df %>%
-         select(logsales) %>%
-         ts(frequency = 52, start = start),
-       xreg = xreg
-  )
-}
-
-
 setenv <- function(name, value) {
   
   # Set R environment variable
