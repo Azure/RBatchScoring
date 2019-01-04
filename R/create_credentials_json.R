2eb515d660f626b0db80ed3c926b5494289e2fdd
angusrtaylor <angusrtaylor@gmail.com>
2019-01-04 14:36:12 +0000

    update json functions and dockerfile

--- R/create_credentials_json.R
+++ R/create_credentials_json.R
@@ -1,15 +1,35 @@
+# create_credentials_json <- function() {
+#   
+#   credentials <- list(
+#     batchAccount = list(
+#       url = Sys.getenv("BATCH_ACCOUNT_URL"),
+#       name = Sys.getenv("BATCH_ACCOUNT_NAME"),
+#       key = Sys.getenv("BATCH_ACCOUNT_KEY")
+#     ),
+#     storageAccount = list(
+#       endpointSuffix = Sys.getenv("STORAGE_ENDPOINT_SUFFIX"),
+#       name = Sys.getenv("STORAGE_ACCOUNT_NAME"),
+#       key = Sys.getenv("STORAGE_ACCOUNT_KEY")
+#     )
+#   )
+#   
+#   credentials_json <- toJSON(credentials, auto_unbox = TRUE, pretty = TRUE)
+#   
+#   write(credentials_json, file = file.path("credentials.json"))
+#   
+# }
+
 create_credentials_json <- function() {
   
   credentials <- list(
-    batchAccount = list(
-      url = Sys.getenv("BATCH_ACCOUNT_URL"),
-      name = Sys.getenv("BATCH_ACCOUNT_NAME"),
-      key = Sys.getenv("BATCH_ACCOUNT_KEY")
-    ),
-    storageAccount = list(
-      endpointSuffix = Sys.getenv("STORAGE_ENDPOINT_SUFFIX"),
-      name = Sys.getenv("STORAGE_ACCOUNT_NAME"),
-      key = Sys.getenv("STORAGE_ACCOUNT_KEY")
+    servicePrincipal = list(
+      tenantId = Sys.getenv("TENANT_ID"),
+      storageEndpointSuffix = Sys.getenv("STORAGE_ENDPOINT_SUFFIX"),
+      batchAccountResourceId = Sys.getenv("BATCH_ACCOUNT_RESOURCE_ID"),
+      storageAccountResourceId = Sys.getenv("STORAGE_ACCOUNT_RESOURCE_ID"),
+      credential = Sys.getenv("SP_PASSWORD"),
+      clientId = Sys.getenv("SP_APP")
+      
     )
   )
   
