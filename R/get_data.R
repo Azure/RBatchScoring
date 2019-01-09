2b5d57d34cf8766dad735751922aee272e94fe65
angusrtaylor <angusrtaylor@gmail.com>
2019-01-09 16:11:29 +0000

    fix file naming

--- R/get_data.R
+++ R/get_data.R
@@ -2,6 +2,9 @@ library(bayesm)
 library(dplyr)
 library(tidyr)
 
+source("R/options.R")
+source("R/utilities.R")
+
 
 print("Extracting data from bayesm package...")
 
@@ -29,6 +32,25 @@ dat <- dat %>%
   )
 
 
+# Reset store names
+
+stores <- dat %>%
+  select(store) %>%
+  distinct()
+
+stores$store_new <- as.integer(row.names(stores))
+
+dat <- dat %>%
+  merge(stores) %>%
+  mutate(store = store_new) %>%
+  select(-store_new)
+
+
+# Set first week to be 0
+
+dat$week <- dat$week - min(dat$week)
+
+
 # Inserting records for missing time periods
 
 get_stores_brands <- function(dat) {
@@ -37,6 +59,7 @@ get_stores_brands <- function(dat) {
     select(store, brand) %>%
     distinct()
   split(df, seq(nrow(df)))
+  
 }
 
 insert_periods <- function(dat) {
@@ -73,14 +96,10 @@ fill_time_series <- function(dat) {
     fill(-c(store, brand, week), .direction = "down") %>%
     ungroup()
   as.data.frame(dat)
+  
 }
 
 dat <- fill_time_series(dat)
-
-
-# Set first week to be 0
-
-dat$week <- dat$week - min(dat$week)
 
 
 # Transform and rename variables
@@ -88,7 +107,7 @@ dat$week <- dat$week - min(dat$week)
 dat$sales <- exp(dat$logmove)
 dat$logmove <- NULL
 
-dat$product <- dat$brand
+dat$product <- 1
 dat$sku <- dat$brand
 dat$brand <- NULL
 
@@ -107,73 +126,39 @@ dat <- dat %>%
 
 # Create data directories
 
-dir.create("data")
+create_dir("data")
+create_dir("models")
+create_dir(file.path("data", "futurex"))
+create_dir(file.path("data", "history"))
+create_dir(file.path("data", "forecasts"))
 
 
-# Split data by product and write to disk
+# Split dataset into history and future features
 
-lapply(unique(dat$product), function(p) {
-  write.csv(
-    dat %>% filter(product == p),
-    file = file.path("data", paste0("product_", p, ".csv")),
-    quote = FALSE,
-    row.names = FALSE
-  )
-})
-#write.csv(dat, file = file.path("data", "small.csv"),
-#          quote = FALSE, row.names = FALSE)
+max_week <- max(dat$week)
+
+history <- dat %>%
+  filter(week <= max_week - FORECAST_HORIZON) %>%
+  arrange(product, sku, store, week)
+
+futurex <- dat %>%
+  filter(week > max_week - FORECAST_HORIZON) %>%
+  select(-c(sales))
+
+
+# Write data to disk
+
+write.csv(
+  history, file.path("data", "history", "1.csv"),
+  quote = FALSE, row.names = FALSE
+)
+
+write.csv(
+  futurex, file.path("data", "futurex", ".csv"),
+  quote = FALSE, row.names = FALSE
+)
+
+rm(dat, orangeJuice, stores, max_week)
+gc()
 
 print("Done")
-
-# # Save training data (reserve last 4 weeks for scoring)
-# 
-# train <- dat %>% filter(week <= max(dat$week) - 4)
-# 
-# write.csv(train, file = file.path("data", "training", "train.csv"),
-#           quote = FALSE, row.names = FALSE)
-# 
-
-# # Expand scoring data
-# 
-# print("Expanding scoring data...")
-# 
-# # Reserve the last 4 weeks for scoring, plus a further 8 weeks for lagged features
-# 
-# scoring <- dat %>% filter(week > max(dat$week) - 12)
-# 
-# 
-# # Replicate brands to ~40,000 skus (https://www.marketwatch.com/story/grocery-stores-carry-40000-more-items-than-they-did-in-the-1990s-2017-06-07)
-# 
-# multiplier <- floor(40000 / length(unique(dat$product)))
-# 
-# max_week <- max(scoring$week)
-# 
-# system.time({
-#   
-#   pb = txtProgressBar(min = 0, max = multiplier, initial = 0)
-#   
-#   for (m in 1:multiplier) {
-#     
-#     #if (m %% 100 == 0) print(paste("Generated", m, "of", multiplier, "products..."))
-#     setTxtProgressBar(pb, m)
-#     
-#     recent_history <- scoring %>%
-#       filter(week <= max_week - 4) %>%
-#       select(product, sku, store, week, sales) %>%
-#       mutate(product = m)
-#     
-#     write.csv(recent_history, file.path("data", "scoring", "history", paste0(m, ".csv")),
-#               quote = FALSE, row.names = FALSE)
-#     
-#     futurex <- scoring %>%
-#       filter(week > max_week - 4) %>%
-#       select(-sales) %>%
-#       mutate(product = m)
-#     
-#     write.csv(futurex, file.path("data", "scoring", "futurex", paste0(m, ".csv")),
-#               quote = FALSE, row.names = FALSE)
-#     
-#   }
-#   
-# })
-# 
