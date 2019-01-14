86f57623029995a7bf15f6deef148bd631036aaf
angusrtaylor <angusrtaylor@gmail.com>
2019-01-14 15:37:12 +0000

    fixed upload_artefacts

--- R/upload_artefacts.R
+++ R/upload_artefacts.R
@@ -2,9 +2,10 @@
 library(AzureRMR)
 library(AzureStor)
 
+source("R/options.R")
 source("R/utilities.R")
 
-key <- ""
+key <- Sys.getenv("HAPPYPATHSKEY")
 
 ep <- blob_endpoint(
   "https://happypathspublic.blob.core.windows.net",
@@ -15,6 +16,17 @@ list_blob_containers(ep)
 assets <- blob_container(ep, "assets")
 
 list_blobs(assets)
+
+
+# Delete existing models
+
+model_names <- list_model_names(list_required_models(6, QUANTILES))
+lapply(model_names, function(m) {
+    delete_blob(assets, paste0("batch_forecasting/models", m), confirm = FALSE)
+  })
+
+
+# Upload new models
 
 run(
   "azcopy --source %s --destination %s --dest-key %s --recursive --quiet",
@@ -35,7 +47,9 @@ upload_to_url(
   src = "forecasts.png",
   dest = file.path(
     "https://happypathspublic.blob.core.windows.net",
-    "assets", "batch_forecasting", "images"
+    "assets", "batch_forecasting", "images", "forecasts.png"
   ),
   key = key
 )
+
+list_blobs(assets)
