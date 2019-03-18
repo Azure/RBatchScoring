59477304300520a03b2f25035648b6fd21af726e
angusrtaylor <angusrtaylor@gmail.com>
2019-03-18 16:05:13 +0000

    add 04 comments

--- 04_run_from_docker.R
+++ 04_run_from_docker.R
@@ -1,13 +1,8 @@
 
-# 05_run_job_from_docker.R
+# 04_run_from_docker.R
 # 
 # This script defines a docker image for the job scheduler. The forecast
 # generation process is then triggered from a docker container running locally.
-#
-# Note: it can take a few minutes for the cluster created in the previous script
-# to be deleted. You will receive an error if you run this script while the
-# previous cluster is being deleted. You can check the status of the cluster
-# by checking the Pools pane of the Batch Account in the Azure Portal.
 #
 # Run time ~5 minutes on a 5 node cluster
 
@@ -28,11 +23,17 @@ run(
   paste0(get_env("DOCKER_ID"), "/", get_env("SCHEDULER_CONTAINER_IMAGE"))
 )
 
+
+# Tag the image
+
 run(
   "sudo docker tag %s:latest %s",
   paste0(get_env("DOCKER_ID"), "/", get_env("SCHEDULER_CONTAINER_IMAGE")),
   paste0(get_env("DOCKER_ID"), "/", get_env("SCHEDULER_CONTAINER_IMAGE"))
 )
+
+
+# Push the image to Docker Hub
 
 run("sudo docker push %s",
     paste0(get_env("DOCKER_ID"), "/", get_env("SCHEDULER_CONTAINER_IMAGE"))
