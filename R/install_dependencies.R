09c8fe51252eae8bb7c0d19ee87a2592123ae843
angusrtaylor <angusrtaylor@gmail.com>
2019-03-23 09:46:13 +0000

    update dependencies

--- R/install_dependencies.R
+++ R/install_dependencies.R
@@ -12,23 +12,28 @@ pkgs <- c("bayesm", "dplyr", "tidyr", "ggplot2")
 install.packages(pkgs, repos = repo)
 
 devtools::install_github(
+  "cloudyr/AzureAuth",
+  ref = "e638802cd588bd90d6d090dbe0b974493bbb6f27" # 2019-03-22
+)
+
+devtools::install_github(
   "cloudyr/AzureRMR",
-  ref = "5b54604ca63e5e0154318f04cc1056f8f946fcde"
+  ref = "5eeb60988a2079b6b320af8df306c4fad802c13c" # 2019-03-23
 )
 
 devtools::install_github(
   "cloudyr/AzureStor",
-  ref = "f7886f2a7e5f0f26100060c94f88a8171ad54782"
+  ref = "79d0c80ff151ec5efc44a8133b465aa0fc0daabe" # 2019-03-21
 )
 
 devtools::install_github(
   "Azure/rAzureBatch",
-  ref = "6ca2bf7b1f4433a27531eaa86f0317499e9b4987" # 2018-08-09
+  ref = "1ab39ca1bb8ae589a6f5c80f5d91c1ee79b1ee8a" # 2019-28-14
 )
 
 devtools::install_github(
   "Azure/doAzureParallel",
-  ref = "6d14d4522b1ff4218f19549b89dea6419a230a53" # 2018-11-26
+  ref = "975858072e8194d465a1f63262e35815ebbf0306" # 2019-02-14
 )
 
 devtools::install_github(
